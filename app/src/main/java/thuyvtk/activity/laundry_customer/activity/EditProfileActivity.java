6d5ecdcfaa4bd6b86601314267fe906e0cd13457
Nhat Thien <thienvo0211@gmail.com>
2019-10-14 20:46:27 +0700

    [ThuyVTK] edit profile

---
+++ app/src/main/java/thuyvtk/activity/laundry_customer/activity/EditProfileActivity.java
@@ -0,0 +1,210 @@
+package thuyvtk.activity.laundry_customer.activity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import thuyvtk.activity.laundry_customer.R;
+import thuyvtk.activity.laundry_customer.library.SharePreferenceLib;
+import thuyvtk.activity.laundry_customer.model.CustomerDTO;
+import thuyvtk.activity.laundry_customer.presenter.CustomerPresenter;
+import thuyvtk.activity.laundry_customer.presenter.StorePresenter;
+import thuyvtk.activity.laundry_customer.view.CustomerView;
+
+
+public class EditProfileActivity extends Activity implements CustomerView {
+    ImageButton imgBackActivity;
+    CustomerDTO currentUser;
+    TextView edtUsername;
+    TextView txtPhone;
+    Button btnEditLocation;
+    EditText edtEmail;
+    Button btnUpdateStore;
+    ImageButton btnSelectImage;
+    LinearLayout ln_waiting;
+    private static final int SELECT_IMAGE = 100;
+    Uri imageUri;
+    CircleImageView image_profile;
+    String imageName;
+    static final String CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=sqlvadtabpe45ilkho;AccountKey=Q0GtVfudYOKaYykP6CLCyk7uG/0Dak6C9WuAGDj5wQizMJDFEtEPaTGkGtdmNAatlbSXo4xznJAvOw4slPYAIg==;EndpointSuffix=core.windows.net";
+    static final String IMAGE_FOLDER = "imagefolder";
+    final String serverName = "https://sqlvadtabpe45ilkho.blob.core.windows.net/";
+    CustomerPresenter customerPresenter;
+    SharePreferenceLib sharePreferenceLib;
+    boolean flagChangeImageProfile = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+        defineView();
+        sharePreferenceLib = new SharePreferenceLib(getApplicationContext());
+//        storePresenter = new StorePresenter(this);
+        getStoreProfile();
+        backPreActivity();
+        selectProfileImage();
+        updateStoreProfile();
+    }
+
+    public void defineView() {
+        imgBackActivity = findViewById(R.id.imgBackActivity);
+        edtUsername = findViewById(R.id.edtUsername);
+        txtPhone = findViewById(R.id.txtPhone);
+        edtEmail = findViewById(R.id.edtEmail);
+        btnEditLocation = findViewById(R.id.btnEditLocation);
+        btnUpdateStore = findViewById(R.id.btnUpdateStore);
+        btnSelectImage = findViewById(R.id.btnSelectImage);
+        image_profile = findViewById(R.id.image_profile);
+        ln_waiting = findViewById(R.id.ln_waiting);
+    }
+
+    public void backPreActivity() {
+        imgBackActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditProfileActivity.this.finish();
+            }
+        });
+    }
+
+    private void getStoreProfile() {
+        SharePreferenceLib sharePreferenceLib = new SharePreferenceLib(this);
+        currentUser = sharePreferenceLib.getUser();
+        edtUsername.setText(currentUser.getCustomerName());
+        txtPhone.setText(currentUser.getPhone());
+        edtEmail.setText(currentUser.getEmail());
+//        Picasso.with(this).load(sharePreferenceLib.getUser().getImageUrl()).into(image_profile);
+    }
+
+    private void selectProfileImage() {
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
+                flagChangeImageProfile = true;
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch (requestCode) {
+            case SELECT_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    this.imageUri = imageReturnedIntent.getData();
+                    this.image_profile.setImageURI(this.imageUri);
+                }
+        }
+    }
+
+    private void uploadImage() {
+        try {
+            final InputStream imageStream = getContentResolver().openInputStream(this.imageUri);
+            final int imageLength = imageStream.available();
+            final Handler handler = new Handler();
+            Thread th = new Thread(new Runnable() {
+                public void run() {
+                    try {
+//                        imageName = ImageManager.UploadImage(imageStream, imageLength, CONNECTION_STRING, IMAGE_FOLDER);
+                        handler.post(new Runnable() {
+                            public void run() {
+                                updateStoreAPI();
+                            }
+
+                        });
+                    } catch (Exception ex) {
+                        handler.post(new Runnable() {
+                            public void run() {
+                                Toast.makeText(getApplicationContext(), "Upload image failed!", Toast.LENGTH_SHORT).show();
+                                image_profile.setImageURI(null);
+                                imageName = "*";// if upload fail set imagename to *
+                            }
+                        });
+                    }
+                }
+            });
+            th.start();
+
+        } catch (Exception ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateStoreProfile() {
+        btnUpdateStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validData()) {
+                    if (flagChangeImageProfile) {
+                        uploadImage();
+                    } else {
+                        updateStoreAPI();
+                    }
+                }
+            }
+        });
+    }
+
+    private void updateStoreAPI() {
+        String imageURL = serverName + IMAGE_FOLDER + "/" + imageName;
+        CustomerDTO customerDTO = sharePreferenceLib.getUser();
+        customerDTO.setCustomerName(edtUsername.getText().toString());
+        customerDTO.setEmail(edtEmail.getText().toString());
+        if (flagChangeImageProfile) {
+//            customerDTO.setImageUrl(imageURL);
+        }
+        updateStore(customerDTO);
+    }
+
+    boolean flag = false;
+
+    private void updateStore(CustomerDTO dto) {
+        if (!flag) {
+//            customerPresenter.updateCustomer(dto);
+            ln_waiting.setVisibility(View.VISIBLE);
+            flag = true;
+        }
+
+    }
+
+    private boolean validData() {
+        if (edtUsername.getText().toString().isEmpty()
+                || edtEmail.getText().toString().isEmpty()) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("Some data are empty!");
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void returnCustomer(CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void updateSuccess() {
+
+    }
+}
--- app/src/main/java/thuyvtk/activity/laundry_customer/fragment/AccountFragment.java
+++ app/src/main/java/thuyvtk/activity/laundry_customer/fragment/AccountFragment.java
@@ -10,9 +10,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import thuyvtk.activity.laundry_customer.R;
 import thuyvtk.activity.laundry_customer.activity.EditAccountActivity;
+import thuyvtk.activity.laundry_customer.activity.EditProfileActivity;
 import thuyvtk.activity.laundry_customer.activity.LoginActivity;
 import thuyvtk.activity.laundry_customer.library.SharePreferenceLib;
 import thuyvtk.activity.laundry_customer.model.OrderDTO;
@@ -22,47 +28,57 @@ import thuyvtk.activity.laundry_customer.view.OrderHistoryView;
  * A simple {@link Fragment} subclass.
  */
 public class AccountFragment extends Fragment implements OrderHistoryView {
-
+    ImageButton imgEdit;
+    CircleImageView image_profile;
+    TextView txtLogout;
     public AccountFragment() {
         // Required empty public constructor
     }
 
     private void defineView(View view) {
+        imgEdit = view.findViewById(R.id.imgEdit);
+        image_profile = view.findViewById(R.id.image_profile);
+        txtLogout = view.findViewById(R.id.txtLogout);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_account, container, false);
         defineView(view);
-//        btnAccount.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(getContext(), EditAccountActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        // logout
-//        btnLogout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                SharePreferenceLib sharePreferenceLib =  new SharePreferenceLib(getContext());
-//                sharePreferenceLib.logouṭ();
-//                Intent intent = new Intent(getContext(),LoginActivity.class);
-//                startActivity(intent);
-//                getActivity().finish();
-//            }
-//        });
-//        btnHistory.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
+        openPageEdit();
+        logOut();
         return view;
-        // view history
+    }
 
+    private void logOut() {
+        txtLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharePreferenceLib sharePreferenceLib =  new SharePreferenceLib(getContext());
+                sharePreferenceLib.logouṭ();
+                Intent intent = new Intent(getContext(),LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+    }
+
+    private void openPageEdit() {
+        image_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        imgEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
